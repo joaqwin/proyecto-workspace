51c5459ed6520304bc48a7f703b45439d6c012bd
joaqwin <joacopm15@gmail.com>
2022-08-20 13:42:19 -0300

    subiendo
    archivos

---
+++ js/login.js
@@ -0,0 +1,20 @@
+let micontraseña = document.getElementById('contraseña');
+let miEmail = document.getElementById('email');
+let miButton= document.getElementById('boton_submit');
+
+miButton.addEventListener('click', function(e){
+    e.preventDefault();
+    let contra = micontraseña.value.length;
+    let email = miEmail.value.length;
+    if (email > 0 && contra > 0){
+        e.preventDefault
+        window.location.href = 'menu.html';
+    }else{
+        e.preventDefault();
+        alert("Los campos no pueden estar vacios")
+           document.getElementById('email').style.borderColor="red";
+           document.getElementById('contraseña').style.borderColor="red";
+            
+    }
+
+})
---
+++ js/products.js
@@ -0,0 +1,40 @@
+lista_de_autos = [];
+
+function showCategoriesList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let category = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ category.name +` - `+ category.currency +` `+category.cost +`</h4> 
+                        <p> `+ category.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + category.soldCount + ` vendidos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    console.log('Se cargó el DOM');
+    getJSONData(AUTOS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            lista_de_autos = resultObj.data;
+            showCategoriesList(lista_de_autos.products);
+        }
+    });
+});
